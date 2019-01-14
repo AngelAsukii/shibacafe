afb68c006dea21a2b5952e3889a4ebfb8dd1c9cb
AngelAsukii <42059981+AngelAsukii@users.noreply.github.com>
2019-01-14 02:43:01 +0100

    Update main.js

--- .gitignore/main.js
+++ .gitignore/main.js
@@ -8,11 +8,48 @@ const ytdl = require('ytdl-core');
 const queue = new Map();
 const rainbowrole = require('./rainbowrole.json');
 const randomColour = require("randomcolor");
+const size    = rainbowrole.colors;
+const rainbow = new Array(size);
 
+for (var i=0; i<size; i++) {
+  var red   = sin_to_hex(i, 0 * Math.PI * 2/4); // 0   deg
+  var blue  = sin_to_hex(i, 1 * Math.PI * 1/4); // 120 deg
+  var green = sin_to_hex(i, 2 * Math.PI * 3/4); // 240 deg
+
+  rainbow[i] = '#'+ red + blue + green;
+}
+
+function sin_to_hex(i, phase) {
+  var sin = Math.sin(Math.PI / size * 3 * i + phase);
+  var int = Math.floor(sin * 107) + 128;
+  var hex = int.toString(16);
+
+  return hex.length === 1 ? '0'+hex : hex;
+}
+
+let place = 0;
+const servers = rainbowrole.servers;
+
+function changeColor() {
+  for (let index = 0; index < servers.length; ++index) {		
+    bot.guilds.get(servers[index]).roles.find('name', rainbowrole.roleName).setColor(rainbow[place])
+		.catch(console.error);
+		
+    if(rainbowrole.logging){
+      console.log(`[ColorChanger] Changed color to ${rainbow[place]} in server: ${servers[index]}`);
+    }
+    if(place == (size - 1)){
+      place = 0;
+    }else{
+      place++;
+    }
+  }
+}
 
 bot.on('ready', () => {
     bot.user.setActivity("//help | Nage dans sa tasse de thé");
     bot.user.setStatus("dnd");
+    setInterval(changeColor, rainbowrole.speed);
     console.log("Bot Ready !");
 });
 
